50c02f10d1a83b94be07c07a8c4bf378ce156ca6
Johan Schuijt <johan.schuijt@ftm.nl>
2024-12-02 18:42:33 +0100

    add type animation to header

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,11 +1,36 @@
-<script>
+<script lang="ts">
 	import '@fontsource-variable/moderustic/index.css';
 	import '@fontsource-variable/lora/index.css';
+
+	import { onMount } from 'svelte';
+
+	export let text: string = '> resolve';
+	export let speed: number = 50;
+
+	let displayed_text: string = '> ';
+	let is_complete: boolean = false;
+
+	function type_text() {
+		if (displayed_text.length < text.length) {
+			displayed_text += text[displayed_text.length];
+			setTimeout(type_text, speed);
+		} else {
+			is_complete = true;
+		}
+	}
+
+	onMount(() => {
+		const timeoutId = setTimeout(type_text);
+
+		return () => {
+			clearTimeout(timeoutId);
+		};
+	});
 </script>
 
 <article>
 	<main>
-		<h1>> resolve</h1>
+		<h1 class="typing-animation" class:complete={is_complete}>{displayed_text}</h1>
 		<p>
 			We are IT professionals with a vision. That vision is for technology to empower humans.
 			Empower them to make sense of the void, empower them to find answers, and empower them to dare
@@ -56,5 +81,14 @@
 
 	a:hover {
 		color: #1a401c;
+	}
+
+	.typing-animation {
+		border-right: 2px solid currentColor;
+		display: inline;
+	}
+
+	.typing-animation.complete {
+		border-right: none;
 	}
 </style>
