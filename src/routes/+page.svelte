03e58891fe4111865e580a4e44af7e1deda2be1e
Johan Schuijt <johan.schuijt@ftm.nl>
2024-12-02 18:34:05 +0100

    add fonts and text

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,60 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script>
+	import '@fontsource-variable/moderustic/index.css';
+	import '@fontsource-variable/lora/index.css';
+</script>
+
+<article>
+	<main>
+		<h1>> resolve</h1>
+		<p>
+			We are IT professionals with a vision. That vision is for technology to empower humans.
+			Empower them to make sense of the void, empower them to find answers, and empower them to dare
+			to ask questions. Will you explore with us? <a href="mailto:johan@resolve.works">Contact us</a
+			>.
+		</p>
+		<br />
+	</main>
+</article>
+
+<style>
+	:global {
+		h1,
+		h2,
+		h3,
+		h4,
+		h5,
+		h6 {
+			font-family: 'Moderustic Variable', serif;
+		}
+
+		body {
+			font-family: 'Lora Variable', serif;
+			margin: 0;
+			line-height: 1.6rem;
+		}
+	}
+
+	h1 {
+		font-weight: normal;
+		font-size: 2rem;
+	}
+
+	article {
+		min-height: 100vh;
+		display: flex;
+		align-items: center;
+		justify-content: center;
+	}
+
+	main {
+		max-width: 60rem;
+	}
+
+	a {
+		color: #2c6c2f;
+	}
+
+	a:hover {
+		color: #1a401c;
+	}
+</style>
